cd2e0d9c4ca421d460da252e6fb20f6dbc4091a1
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-05-30 18:08:47 +0530

    update

--- todo/app/index.js
+++ todo/app/index.js
@@ -1,17 +1,67 @@
+// import { useState } from "react"
+// import { TextInput, View, Text, Alert, Button } from "react-native"
+
+// export default function App() {
+
+//     const names = ["name1", "name2", "name3", "name4", "name5", "name6"]
+
+//     return (
+//         <View>
+//             {names.map((item,index) => {
+//                 return(
+//                 <Text key={index}>{index + 1}. {item}</Text>
+//             )
+//             })}
+//         </View>
+//     )
+// }
+
 import { useState } from "react"
-import { TextInput, View, Text, Alert, Button } from "react-native"
+import { View, Text, TextInput, alert, Button } from "react-native"
 
 export default function App() {
+  const arrobject = [
+    {
+      title: "Rohit",
+      age: 27,
+      roll: 26,
+    },
+    {
+      title: "Rohit2",
+      age: 33,
+      roll: 56,
+    },
+    {
+      title: "Rohit3",
+      age: 55,
+      roll: 12,
+    },
+    {
+      title: "Rohit4",
+      age: 89,
+      roll: 22,
+    }
+  ]
+  const arrobjects = [
+  ];
 
-    const names = ["name1", "name2", "name3", "name4", "name5", "name6"]
+  return (
+    <View>
+      {arrobject.filter((item, index) => {
+        return index > 0;
+      }).map((item, index) => {
+          return (
+            <View key={index}>
+              <Text>
+                {item.title}
+                {/* {item.title} */}
 
-    return (
-        <View>
-            {names.map((item,index) => {
-                return(
-                <Text key={index}>{index + 1}. {item}</Text>
-            )
-            })}
-        </View>
-    )
+              </Text>
+            </View>
+          )
+        
+      })
+      }
+    </View>
+  );
 }
